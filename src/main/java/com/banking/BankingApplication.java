7d903929bef0a57f1acc5736a66998b29928ea6d
Frank D <durso135@gmail.com>
2022-04-30 10:24:02 -0700

    troubleshooting CORS on deployment

--- src/main/java/com/banking/BankingApplication.java
+++ src/main/java/com/banking/BankingApplication.java
@@ -27,7 +27,7 @@ public class BankingApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200, http://revature-bank-runner.s3-website-us-east-1.amazonaws.com:80"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200, http://revature-bank-runner.s3-website-us-east-1.amazonaws.com"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
