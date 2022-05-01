29ae7d025622d9d0252ffce7d987923a8afe9ef0
Frank D <durso135@gmail.com>
2022-05-01 12:38:30 -0700

    test for CORS

--- src/main/java/com/banking/BankingApplication.java
+++ src/main/java/com/banking/BankingApplication.java
@@ -1,23 +1,14 @@
 package com.banking;
 
-import com.banking.models.checkingAccountInfo;
-import com.banking.services.checkingServiceimpl;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import com.banking.repositories.checkingAccountInfoRepo;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
-import java.util.Arrays;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 @RestController
-
 public class BankingApplication {
 
 	public static void main(String[] args)
@@ -27,20 +18,6 @@ public class BankingApplication {
 		SpringApplication.run(BankingApplication.class, args);
 
 	}
-	@Bean
-	public CorsFilter corsFilter() {
-		CorsConfiguration corsConfiguration = new CorsConfiguration();
-		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://revature-bank-runner.s3-website-us-east-1.amazonaws.com"));
-		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
-				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
-				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
-		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
-				"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
-		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
-		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
-		return new CorsFilter(urlBasedCorsConfigurationSource);
-	}
+
 
 }
